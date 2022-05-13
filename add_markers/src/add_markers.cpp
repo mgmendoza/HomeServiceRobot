e062de9ef0df279b642ff54ee79e2def8565811d
Maria Mendoza <mgmendoza@alumni.usc.edu>
2022-05-12 17:09:19 -0700

    added statement to remove marker from pickup location

--- add_markers/src/add_markers.cpp
+++ add_markers/src/add_markers.cpp
@@ -29,6 +29,17 @@
 
 #include <ros/ros.h>
 #include <visualization_msgs/Marker.h>
+#include "nav_msgs/Odometry.h"
+#include <cmath>
+
+double pose[2] = {0.0, 0.0};
+
+void getOdom(const nav_msgs::Odometry::ConstPtr& msg)
+{
+  pose[0] = msg->pose.pose.position.x;
+  pose[1] = msg->pose.pose.position.y;
+}
+
 
 int main( int argc, char** argv )
 {
@@ -36,12 +47,17 @@ int main( int argc, char** argv )
   ros::NodeHandle n;
   ros::Rate r(1);
   ros::Publisher marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 1);
-
-  // Set our initial shape type to be a cube
+  ros::Subscriber odomSub = n.subscribe("odom", 10, getOdom);
+  
+  
+  // Set our shape type to be a cube
   uint32_t shape = visualization_msgs::Marker::CUBE;
+  
 
   while (ros::ok())
   {
+
+
     visualization_msgs::Marker marker;
     // Set the frame ID and timestamp.  See the TF tutorials for information on these.
     marker.header.frame_id = "map";
@@ -78,9 +94,11 @@ int main( int argc, char** argv )
     marker.color.b = 0.8f;
     marker.color.a = 1.0;
 
-    marker.lifetime = ros::Duration();
+    //marker.lifetime = ros::Duration();
+    marker.lifetime  = ros::Duration(0.4);
 
     // Publish the marker
+    /*
     while (marker_pub.getNumSubscribers() < 1)
     {
       if (!ros::ok())
@@ -90,10 +108,38 @@ int main( int argc, char** argv )
       ROS_WARN_ONCE("Please create a subscriber to the marker");
       sleep(1);
     }
-    
-    
+   */ 
 
     marker_pub.publish(marker);
+
+    if (pose[0] < -3.0 && pose[1] < -2.0){
+        marker.action = visualization_msgs::Marker::DELETE; 
+        uint32_t shape = visualization_msgs::Marker::SPHERE;
+        
+       marker.action = visualization_msgs::Marker::ADD;
+       marker.pose.position.y = -2.0;
+       marker.pose.position.z = 0;
+       marker.pose.orientation.x = 0.0;
+       marker.pose.orientation.y = 0.0;
+       marker.pose.orientation.z = 0.0;
+       marker.pose.orientation.w = 1.0;
+
+       // Set the scale of the marker -- 1x1x1 here means 1m on a side
+       marker.scale.x = 0.5;
+       marker.scale.y = 0.5;
+       marker.scale.z = 0.5;
+
+       // Set the color -- be sure to set alpha to something non-zero!
+       marker.color.r = 1.0f;
+       marker.color.g = 0.0f;
+       marker.color.b = 0.0f;
+       marker.color.a = 1.0;
+
+       //marker.lifetime = ros::Duration();
+       marker.lifetime  = ros::Duration(10.0);
+
+    }
+
 
     /*
     // Cycle between different shapes
